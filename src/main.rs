878125e4d4991efda41dfa813158c1ccdecd253b
aninarafath <arafathanin6@gmail.com>
2026-02-14 02:01:18 +0530

    prefix search impl

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
-use std::collections::HashMap;
+use std::{collections::HashMap};
 
-trait Trie {
-    fn insert(&self, string:&String);
-    fn serach(&self, string:&String);
-}
 struct TrieNode {
     children: HashMap<char, TrieNode>,
     is_terminal_node: bool,
@@ -17,22 +13,65 @@ impl TrieNode {
         }
     }
 
-    fn insert(&mut self, new_str: &str) {
+    fn insert(&mut self, word: &str) {
         let mut current_node = self;
-        for c in new_str.chars() {
-            if !current_node.children.contains_key(&c) {
-                current_node.children.insert(c, TrieNode::new());
-            }
-            current_node = current_node.children.get_mut(&c).unwrap();
+        for c in word.chars() {
+            current_node = current_node.children.entry(c).or_insert(TrieNode::new());
         }
         current_node.is_terminal_node = true
+    }
+
+    fn search(&self, word: &str) -> bool {
+        let mut current_node = self;
+        for c in word.chars() {
+            if let Some(node) = current_node.children.get(&c) {
+                current_node = node
+            } else {
+                return false;
+            }
+        }
+        return current_node.is_terminal_node;
+    }
+
+    fn prefix(&self, prefix: &str) -> Vec<String> {
+        let mut current = self;
+
+        for c in prefix.chars() {
+            match current.children.get(&c) {
+                Some(node) => current = node,
+                None => return Vec::new(),
+            }
+        }
+
+        let mut results = Vec::new();
+        let mut current_word = prefix.to_string();
+
+
+        Self::dfs(&current,&mut current_word,&mut results);
+
+        return results;
+    }
+
+    fn dfs(node:&TrieNode,current_word:&mut String,results:&mut Vec<String>){
+
+        if node.is_terminal_node{
+            results.push(current_word.clone());
+        }
+
+        for (ch,child_node) in &(node.children){
+            current_word.push(*ch);
+            Self::dfs(child_node, current_word, results);
+            current_word.pop();
+        }
     }
 }
 
 fn main() {
-    let sample_text = "anin";
-
+    let sample_text = "test";
     let mut trie = TrieNode::new();
-
     trie.insert(sample_text);
+    trie.insert("anin");
+    trie.insert("animal");
+    trie.insert("animation");
+    print!("have {:?}\n", trie.prefix("ani"));
 }
